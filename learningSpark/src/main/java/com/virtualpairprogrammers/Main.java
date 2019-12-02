da18bc8fec41daa79711ce9f01db87ab87815352
Thomas SCHWENDER <ardemius@gmail.com>
2019-12-02 02:34:14 +0100

    updates notes up to lecture 13

--- learningSpark/src/main/java/com/virtualpairprogrammers/Main.java
+++ learningSpark/src/main/java/com/virtualpairprogrammers/Main.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,13 @@ public class Main {
         // just to avoid too many unnecessary logs
         // Be careful to use a org.apache.log4j.Logger and no other!
         Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+//        upToLecture11();
+        upToLectureNext();
+
+    }
+
+    private static void upToLecture11() {
 
         List<Integer> inputData = new ArrayList<>();
         inputData.add(10);
@@ -41,9 +49,21 @@ public class Main {
 
             // 9: Outputting results to the console
             System.out.println("Printing sqrtRDD values through a FOREACH");
+            System.out.println("a) with a classic lambda");
             sqrtRDD.foreach(value -> System.out.println(value));
-            // other possibility
+
+            // other possibility, using a Java 8 reference method
             //sqrtRDD.foreach( System.out::println );
+            // 11: NotSerializableException
+            // Exception in thread "main" org.apache.spark.SparkException: Task not serializable
+            // the previous row will NOT work on my computer as it uses multiple physical CPUs (12 indeed),
+            // thus requiring System.out::println to be Serializable, which it is NOT.
+            // If I only had a computer with a single CPU, this problem would not happen.
+            // To avoid it even in my case, we need to transform our RDD to something else, like a classic Java List
+            // which can be done with a JavaRDDLike.collect method. Once we have a List, everything is "really" local,
+            // and the serialization matter does not exist anymore
+            System.out.println("b) with a method reference AND a PRIOR JavaRDDLike.collect");
+            sqrtRDD.collect().forEach(System.out::println);
 
             // 10: counting Big Data items
             // with a classic JavaRDDLike.count method
@@ -53,8 +73,27 @@ public class Main {
             Long count = mapToOneRDD.reduce((value1, value2) -> value1 + value2);
             System.out.println("Same count given by a mix of map and reduce: " + count);
         }
+    }
+
+    private static void upToLectureNext() {
+
+        List<Integer> inputData = new ArrayList<>();
+        inputData.add(10);
+        inputData.add(11);
+        inputData.add(12);
+        inputData.add(50);
+
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+
+            JavaRDD<Integer> originalIntegers = sc.parallelize(inputData);
+            JavaRDD<Tuple2<Integer, Double>> sqrtRDD = originalIntegers.map(value -> new Tuple2<Integer, Double>(value, Math.sqrt(value)));
+            System.out.println("Printing RDD made of Tuple2 (value and its square root)");
+            sqrtRDD.foreach(value -> System.out.println(value));
 
 
+        }
 
     }
 }
