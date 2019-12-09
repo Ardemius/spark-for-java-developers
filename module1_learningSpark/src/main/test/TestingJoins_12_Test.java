6d93c2768cf42cd8554fd9fb98a27b8c99e72564
Thomas SCHWENDER <ardemius@gmail.com>
2019-12-09 04:00:46 +0100

    rename source folder

---
+++ module1_learningSpark/src/main/test/TestingJoins_12_Test.java
@@ -0,0 +1,14 @@
+import com.virtualpairprogrammers.TestingJoins_12;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestingJoins_12_Test {
+
+    @Test
+    void main() {
+        System.out.println("Just a simple test");
+        TestingJoins_12.main(null);
+
+        Assertions.assertTrue(true);
+    }
+}
